8e5f9fa9ab0a476a80aea5c1d1d3bb829d5c0ba0
alex <alejandrosancarranza@gmail.com>
2024-10-27 19:36:05 -0500

    Cambio de Maven a Gradle

--- src/main/java/com/example/notificationservice/NotificationServiceApplication.java
+++ src/main/java/com/example/notificationservice/NotificationServiceApplication.java
@@ -2,7 +2,9 @@ package com.example.notificationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableJpaRepositories("com.example.notificationservice.repository")
 @SpringBootApplication
 public class NotificationServiceApplication {
 
